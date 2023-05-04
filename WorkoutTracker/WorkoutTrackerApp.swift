f9b383ce2fbee99e0b7e46d2319b8deeaf9d0977
bernie-graves <75196548+bernie-graves@users.noreply.github.com>
2023-05-03 17:35:26 -0700

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Bernie Graves on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
